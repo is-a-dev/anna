b67ff4013b7abad958a516e028f8497485a4bb52
orxngc <orangc@proton.me>
2024-09-07 19:10:39 +0300

    fix maskduck's silly messages in testing functions

--- anna/extensions/testing_functions.py
+++ anna/extensions/testing_functions.py
@@ -9,25 +9,25 @@ class Testings(commands.Cog):
     @commands.is_owner()
     async def hinder(self, ctx: commands.Context, cmd: str):
         if cmd == "hinder":
-            await ctx.send("I did not expect you to be such a fool")
+            await ctx.send("You cannot hinder the hinder command.")
         else:
             command = self._bot.get_command(cmd)
             if command is None:
                 await ctx.send("Command not found.")
                 return
             command.enabled = not command.enabled
-            await ctx.send("Request satisfied, master.")
+            await ctx.send("Successfully hindered ${cmd}.")
 
     @commands.command()
     @commands.is_owner()
     async def test_owner_perm(self, ctx: commands.Context):
-        await ctx.send("Master, how can I help you?")
+        await ctx.send("You have owner level permissions when interacting with Anna.")
 
     @commands.command()
     @commands.is_owner()
     async def reload_slash_command(self, ctx: commands.Context) -> None:
         await ctx.bot.sync_application_commands()
-        await ctx.send("Request satisfied, master.")
+        await ctx.send("Successfully synced bot application commands.")
 
     @commands.command()
     @commands.has_role(830875873027817484)
@@ -35,9 +35,9 @@ class Testings(commands.Cog):
         try:
             self._bot.unload_extension("extensions.oneword")
         except commands.ExtensionNotLoaded:
-            await ctx.send("Oneword cog not already loaded.")
+            await ctx.send("The <#1225794824649838612> was already unloaded.")
         await ctx.send(
-            "Unloaded extension. Please ask my master to reload the cog if needed."
+            "Successfully unloaded the <#1225794824649838612> cog."
         )
 
 
