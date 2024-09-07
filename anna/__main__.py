82b448f19bac7a122bbad5266b5c0e16b02a7343
orxngc <orangc@proton.me>
2024-09-07 16:05:24 +0300

    allow the bot to pong people

--- anna/__main__.py
+++ anna/__main__.py
@@ -80,7 +80,7 @@ bot = AnnaBot(
     help_command=help_commands.PaginatedHelpCommand(),
     case_insensitive=True,
     owner_ids=owner_ids,
-    allowed_mentions=nextcord.AllowedMentions.none(),
+    allowed_mentions=nextcord.AllowedMentions(everyone=False, roles=False, users=True, replied_user=True),
     activity=nextcord.Activity(
         type=nextcord.ActivityType.watching, 
         name="is-a.dev",                      
