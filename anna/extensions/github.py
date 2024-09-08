c65cf192a03606e23a402f66b1cc8e7dca77803a
orxngc <orangc@proton.me>
2024-09-08 15:14:29 +0300

    overhaul github module

--- anna/extensions/github.py
+++ anna/extensions/github.py
@@ -1,33 +1,21 @@
 from __future__ import annotations
-
 import re
 from typing import List
-
 import aiohttp
 import nextcord
 from nextcord.ext import commands
 
-
+# GitHub request helper
 async def request(*args, **kwargs):
     async with aiohttp.ClientSession() as session:
         async with session.request(*args, **kwargs) as ans:
             return await ans.json()
 
-
-# so my LSP know it
-
-request.__doc__ = aiohttp.ClientSession.request.__doc__
-
-
+# Regex patterns
 FULL_MATCH_ANY_REPO = r"(https:\/\/github.com\/)?([A-Za-z1-9-]+)\/([A-Za-z1-9-]+)(\/pull)?(#|\/)(?P<pr_id>\d+)"
-
-
-MATCH_IS_A_DEV_ONLY = r"register#(\d+)"
 VERY_SHORT_MESSAGE = r"##(\d+)"
-SHORT_MATCHES = r"repo:[^\/#]+\/[^#]+#[^, ]+"
 PR_CHANNEL_ID = 1130858271620726784
 STAFF_ROLE_ID = 1197475623745110109
-
 
 class _PRRawObject(object):
     def __init__(self, *, repo_owner: str, repo_name: str, pr_id: str) -> None:
@@ -35,31 +23,25 @@ class _PRRawObject(object):
         self.repo_name = repo_name
         self.pr_id = pr_id
 
-
 class GitHub(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message: nextcord.Message):
-        # if message.channel.id != PR_CHANNEL_ID: return
         if message.author.bot:
             return
-        full_matches: List[re.Match] = re.findall(FULL_MATCH_ANY_REPO, message.content)
-        # print(full_matches)
-        is_a_dev_matches: List[re.Match] = re.findall(  # noqa: F841
-            MATCH_IS_A_DEV_ONLY, message.content
-        )
-        short_matches: List[re.Match] = re.findall(  # noqa: F841
-            SHORT_MATCHES, message.content
-        )
 
-        very_short_matches: List[re.Match] = re.findall(
-            VERY_SHORT_MESSAGE, message.content
-        )
+        # Match for repo format 'repo:owner/repo'
+        repo_matches: List[re.Match] = re.findall(r"repo:([A-Za-z1-9-]+)/([A-Za-z1-9-]+)", message.content)
+        full_matches: List[re.Match] = re.findall(FULL_MATCH_ANY_REPO, message.content)
+        very_short_matches: List[re.Match] = re.findall(VERY_SHORT_MESSAGE, message.content)
+
         pr_list: List[_PRRawObject] = []
+        embed_description = ""  # For final description aggregation
+
+        # Handle PRs/issues from full matches (links or #)
         if len(full_matches) > 0:
             for x in full_matches:
                 if x[0] == "https://github.com/":
                     await message.edit(suppress=True)
-                # print(x)
                 repo_owner = x[1]
                 repo_name = x[2]
                 pr_id = x[5]
@@ -68,45 +50,109 @@ class GitHub(commands.Cog):
                         repo_owner=repo_owner, repo_name=repo_name, pr_id=pr_id
                     )
                 )
+
+        # Handle very short messages (##PRNUMBER) as is-a-dev/register
         if len(very_short_matches) > 0:
-            for x in very_short_matches:
-                pr_id = x
-                repo_owner = "is-a-dev"
-                repo_name = "register"
+            for pr_id in very_short_matches:
                 pr_list.append(
                     _PRRawObject(
-                        repo_owner=repo_owner, repo_name=repo_name, pr_id=pr_id
+                        repo_owner="is-a-dev",
+                        repo_name="register",
+                        pr_id=pr_id
                     )
                 )
 
-        # TODO: Fix is-a-dev-only regex
+        # Only process PR/issue embed once for all PRs/issues
+        if len(pr_list) > 0:
+            for pr in pr_list:
+                try:
+                    i = await request(
+                        "GET",
+                        f"https://api.github.com/repos/{pr.repo_owner}/{pr.repo_name}/issues/{pr.pr_id}",
+                    )
 
+                    # Check if the PR is open, closed, or merged
+                    state = i.get('state', 'open')
+                    merged = i.get('merged', False)
+
+                    # Color based on state
+                    if merged:
+                        color = nextcord.Color.purple()  # Merged
+                    elif state == 'closed':
+                        color = nextcord.Color.red()  # Closed
+                    else:
+                        color = nextcord.Color.green()  # Open
+
+                    # Embed description for the PR/issue
+                    embed_description += f"[(#{pr.pr_id}) {i['title']}]({i['html_url']})\n"
+
+                    # Build and send the embed
+                    embed = nextcord.Embed(
+                        title=f"PR/Issue: {pr.repo_name}",
+                        description=embed_description,
+                        color=color,
+                    )
+                    embed.add_field(name="Status", value=state.title(), inline=True)
+                    await message.channel.send(embed=embed)
+
+                except aiohttp.ClientError as e:
+                    embed_description += f"Error fetching PR #{pr.pr_id}: {str(e)}\n"
+
+            return  # Prevent sending another embed below
+        
         if len(pr_list) == 0:
             if message.channel.id == PR_CHANNEL_ID:
                 if message.author.get_role(STAFF_ROLE_ID) is None:  # type: ignore
                     await message.delete()
 
             return
-        embed_description = """"""
-        for pr in pr_list:
-            i = await request(
-                "GET",
-                f"https://api.github.com/repos/{pr.repo_owner}/{pr.repo_name}/issues/{pr.pr_id}",
-            )
-            embed_description += f"[(#{pr.pr_id}) {i['title']}]({i['html_url']})\n"
+        
+        # Handle repo:owner/repo matches
+        if len(repo_matches) > 0:
+            for repo_owner, repo_name in repo_matches:
+                try:
+                    repo_info = await request(
+                        "GET", f"https://api.github.com/repos/{repo_owner}/{repo_name}"
+                    )
+                    owner_info = await request(
+                        "GET", f"https://api.github.com/users/{repo_owner}"
+                    )
 
-        embed = nextcord.Embed(
-            title="PR/Issue",
-            description=embed_description,
-            color=nextcord.Color.from_rgb(136, 225, 180),
-        )
-        if message.channel == PR_CHANNEL_ID:
-            embed.set_footer(text="No other messages, even 'Please' or 'Thank you'.")
-        await message.channel.send(embed=embed)
+                    # Owner profile image and other details
+                    owner_name = owner_info.get("login", "Unknown")
+                    owner_avatar = owner_info.get("avatar_url", "")
 
-    # @nextcord.slash_command()
-    # async def gh(self, interaction: nextcord.Interaction) -> None:
-    #     pass
+                    # Repository details
+                    repo_url = repo_info.get("html_url", f"https://github.com/{repo_owner}/{repo_name}")
+                    repo_description = repo_info.get("description", "No description available")
+                    stars = repo_info.get("stargazers_count", 0)
+                    forks = repo_info.get("forks_count", 0)
+
+                    # Check for social preview image (as a "banner")
+                    repo_banner = repo_info.get("social_preview_url", "")
+                    topics = repo_info.get("topics", [])
+
+                    # Embedding repository information
+                    embed = nextcord.Embed(
+                        title=f"Repository: {repo_owner}/{repo_name}",
+                        description=f"[Visit Repository]({repo_url})\n{repo_description}",
+                        color=nextcord.Color.blue(),
+                    )
+                    embed.set_thumbnail(url=owner_avatar)
+                    embed.add_field(name="Stars", value=str(stars), inline=True)
+                    embed.add_field(name="Forks", value=str(forks), inline=True)
+
+                    if topics:
+                        embed.add_field(name="Topics", value=", ".join(topics), inline=False)
+
+                    # Add repository banner if available
+                    if repo_banner:
+                        embed.set_image(url=repo_banner)
+
+                    await message.channel.send(embed=embed)
+                except aiohttp.ClientError as e:
+                    await message.channel.send(f"Error fetching repository {repo_owner}/{repo_name}: {str(e)}")
+                    continue
 
 
 def setup(bot: commands.Bot) -> None:
