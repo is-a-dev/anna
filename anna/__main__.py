162e66f161086cc50a7ab966998e90e510cab82e
orxngc <orangc@proton.me>
2024-09-08 13:45:12 +0300

    some cleanup

--- anna/__main__.py
+++ anna/__main__.py
@@ -1,32 +1,20 @@
 from __future__ import annotations
-
 from os import environ
 from typing import Optional
-
 from dotenv import load_dotenv
-
 load_dotenv()
 import os
-
 import nextcord
 from nextcord import ApplicationError, Game, Intents
 from nextcord.ext import application_checks as ac
-from nextcord.ext import commands, help_commands  # type: ignore
-
-from nextcord.ext import tasks
+from nextcord.ext import commands, help_commands, tasks  # type: ignore
 import asyncio
-from nextcord.ext import commands
 import logging
 from extensions.help_forum.database import Database
 import subprocess
 from extensions.help_forum.config import DATABASE_PATH
 
 prefix = "a!" if os.getenv("TEST") else "a?" # bot prefix, first value is for when the bot is in test mode, second is the general prefix
-
-
-class ConvertibleToInt:
-    def __int__(self) -> int: ...
-
 
 class Bot(commands.Bot):
     def __init__(self, db_path: str, *args, **kwargs) -> None:
@@ -70,16 +58,8 @@ class Bot(commands.Bot):
         self.update.start()
 
 
-def convert_none_to_0(key: Optional[ConvertibleToInt] = None) -> int:
-    if key is None:
-        return 0
-    else:
-        return int(key)
-
 
 owner_ids = [716306888492318790 , 961063229168164864] # cutedog and orangc
-# if not convert_none_to_0(os.getenv("TEST")):  # type: ignore[reportArgumentType]
-#     owner_ids.append(853158265466257448)
 intents = Intents.all()
 intents.message_content = True
 intents.members = True
@@ -104,18 +84,13 @@ bot = Bot(
         assets={"large_image": "is-a-dev"}    
     )
 )
-# @bot.event
-# async def on_command_error(ctx, error):
-#     k = await ctx.bot.create_dm(nextcord.Object(id=716134528409665586))
-#     await k.send(traceback.format_exception(error))
-#     print(traceback.format_exception(error))
 
 # TODO: Remove onami when nextcord 3.0 release
 # WARNING: Do not remove this if!
 if nextcord.version_info < (3, 0, 0):
     bot.load_extension("onami")
 
-# bot.load_extension("extensions.help_forum")
+bot.load_extension("extensions.help_forum")
 bot.load_extension("extensions.antihoist")
 bot.load_extension("extensions.fun")
 bot.load_extension("extensions.faq")
