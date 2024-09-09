947c7c9f2c0b00851c125f64cfaf9a9c20ac1abd
orxngc <orangc@proton.me>
2024-09-09 10:00:26 +0300

    fix: FINALLY the stupid suppress bug, and with that all bugs, are finished

--- anna/extensions/help_forum/config.py
+++ anna/extensions/help_forum/config.py
@@ -20,7 +20,7 @@ VIEW_OPEN_LABEL = "Open Thread"
 VIEW_CLOSE_LABEL = "Close Thread"
 
 THREAD_MIN_CHAR = 50
-THREAD_MIN_SUPPRESS_PREFIX = "!sp"
+THREAD_MIN_SUPPRESS_PREFIX = "a?suppress"
 THREAD_MIN_FAIL = f"Your message must be at least {THREAD_MIN_CHAR} characters long. Please provide more detail about the issue which you are facing."
 THREAD_NAME = "Help Thread (member.name)"
 THREAD_EMBED_TITLE = "**Please read this message before you make your first message.**"
--- anna/extensions/help_forum/help_system.py
+++ anna/extensions/help_forum/help_system.py
@@ -452,17 +452,17 @@ class Help(commands.Cog):
             return
         if thread["has_first_message"]:
             return
-        if not message.content(config.THREAD_MIN_SUPPRESS_PREFIX):
+        if not message.content.startswith(config.THREAD_MIN_SUPPRESS_PREFIX):
             if len(message.content) < config.THREAD_MIN_CHAR:
                 await message.reply(config.THREAD_MIN_FAIL)
                 return
-        role = nextcord.utils.get(message.guild.roles, id=guild_config["ping_role_id"])
-        await message.reply(
-            role.mention,
-            delete_after=2,
-            allowed_mentions=nextcord.AllowedMentions(roles=True),
-            flags=nextcord.MessageFlags(suppress_notifications=True),
-        )
+            role = nextcord.utils.get(message.guild.roles, id=guild_config["ping_role_id"])
+            await message.reply(
+                role.mention,
+                delete_after=2,
+                allowed_mentions=nextcord.AllowedMentions(roles=True),
+                flags=nextcord.MessageFlags(suppress_notifications=True),
+            )
         await self.bot.db.set_has_first_message(message.channel.id)
 
 
