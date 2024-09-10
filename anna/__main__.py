9827c93086de9f678f8a33b6e07367ec9339a3f9
orxngc <orangc@proton.me>
2024-09-10 13:48:04 +0300

    error handling!

--- anna/__main__.py
+++ anna/__main__.py
@@ -19,26 +19,6 @@ class Bot(commands.Bot):
         self.db = Database(db_path)
         self.persistent_views_added = False
 
-    async def on_command_error(self, context: commands.Context, error: commands.CommandError) -> None:
-        if isinstance(error, commands.NotOwner):
-            await context.send("Only Anna's maintainers can run this command.")
-        elif isinstance(error, commands.UserInputError):
-            await context.send("User input error.")
-        elif isinstance(error, commands.CommandNotFound):
-            await context.send("Command not found.")
-        elif isinstance(error, commands.errors.DisabledCommand):
-            await context.send("This command has been disabled by Anna's maintainers.")
-        else:
-            await context.send("An error was caught while attempting to run the command.")
-            await super().on_command_error(context, error)
-
-    async def on_application_command_error(self, interaction: nextcord.Interaction, exception: ApplicationError) -> None:
-        if isinstance(exception, ac.ApplicationMissingRole):
-            await interaction.send("You must be a staff member to use this command.")
-        else:
-            await interaction.send("An error was caught while attempting to run the command.")
-            await super().on_application_command_error(interaction, exception)
-
     @commands.Cog.listener()
     async def on_ready(self):
         """ Event triggered when the bot is ready """
@@ -48,7 +28,7 @@ class Bot(commands.Bot):
 bot = Bot(
     db_path=DATABASE_PATH,
     intents=Intents.all(),
-    command_prefix="." if os.getenv("TEST") else "a?",
+    command_prefix="a!" if os.getenv("TEST") else "a?",
     help_command=help_commands.PaginatedHelpCommand(),
     case_insensitive=True,
     owner_ids=[716306888492318790, 961063229168164864],  # Cutedog and orangc
@@ -60,7 +40,7 @@ bot = Bot(
     )
 )
     
-extensions = ["extensions.help_forum.help_system", "extensions.antihoist", "extensions.fun", "extensions.faq", "extensions.antiphishing", "extensions.testing_functions", "extensions.nonsense", "extensions.dns", "extensions.suggestions", "extensions.delete_response", "extensions.github", "extensions.oneword", "extensions.utils", "extensions.tags", "extensions.ping_cutedog"]
+extensions = ["extensions.help_forum.help_system", "extensions.antihoist", "extensions.fun", "extensions.faq", "extensions.antiphishing", "extensions.testing_functions", "extensions.nonsense", "extensions.dns", "extensions.suggestions", "extensions.delete_response", "extensions.github", "extensions.oneword", "extensions.utils", "extensions.tags", "extensions.ping_cutedog", "errors"]
 if nextcord.version_info < (3, 0, 0):
     extensions.append("onami")
 if os.getenv("HASDB"):
