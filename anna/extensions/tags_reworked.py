07bd2b6befa459c1e9a49fb9377a27c43c7c9868
andrewstech <Andrewbuild48@gmail.com>
2024-09-10 21:07:22 +0000

    Switching to Mongo

--- anna/extensions/tags_reworked.py
+++ anna/extensions/tags_reworked.py
@@ -26,25 +26,20 @@
 #OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 from __future__ import annotations
-
 import uuid
-
 import nextcord
-import psycopg2
+from motor.motor_asyncio import AsyncIOMotorClient
 from dotenv import load_dotenv
 from nextcord.ext import application_checks, commands
 
 load_dotenv()
 import os
-from os import getenv
 
-# if you are asking why there's a "my_" before the values, then it's because self.title overlaps the value in modal
-
-
+# Define the MongoDB setup
 class TagEditModal(nextcord.ui.Modal):
-    def __init__(self, db: psycopg2.connection, tag_info: tuple):
-        self._db: psycopg2.connection = db
-        self._tag: tuple = tag_info
+    def __init__(self, db, tag_info: dict):
+        self._db = db
+        self._tag = tag_info
         super().__init__("Edit tag")
 
         self.my_title = nextcord.ui.TextInput(
@@ -52,7 +47,7 @@ class TagEditModal(nextcord.ui.Modal):
             required=True,
             max_length=60,
             style=nextcord.TextInputStyle.short,
-            default_value=self._tag[2],
+            default_value=self._tag["title"],
         )
         self.add_item(self.my_title)
 
@@ -61,50 +56,37 @@ class TagEditModal(nextcord.ui.Modal):
             required=True,
             max_length=4000,
             style=nextcord.TextInputStyle.paragraph,
-            default_value=self._tag[3],
+            default_value=self._tag["content"],
         )
         self.add_item(self.my_content)
 
     async def callback(self, interaction: nextcord.Interaction) -> None:
-        with self._db.cursor() as cursor:
-            try:
-                cursor.execute(
-                    "UPDATE taginfo SET title=%s WHERE id=%s",
-                    (self.my_title.value, self._tag[0]),
-                )
-                cursor.execute(
-                    "UPDATE taginfo SET content=%s WHERE id=%s",
-                    (self.my_content.value, self._tag[0]),
-                )
-            except:
-                cursor.execute("ROLLBACK")
-
-            self._db.commit()
-
+        await self._db.tags.update_one(
+            {"_id": self._tag["_id"]},
+            {"$set": {"title": self.my_title.value, "content": self.my_content.value}},
+        )
         await interaction.response.send_message("Done", ephemeral=True)
 
 
 class TagEditView(nextcord.ui.View):
-    def __init__(self, ctx: commands.Context, db: psycopg2.connection, tag: tuple):
+    def __init__(self, ctx: commands.Context, db, tag: dict):
         super().__init__()
-        self._ctx: commands.Context = ctx
-        self._db: psycopg2.connection = db
-        self._tag: tuple = tag
+        self._ctx = ctx
+        self._db = db
+        self._tag = tag
 
     @nextcord.ui.button(label="Edit tag!", style=nextcord.ButtonStyle.green)
-    async def create_tag(
-        self, button: nextcord.ui.Button, interaction: nextcord.Interaction
-    ):
+    async def create_tag(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
         if interaction.user.id == self._ctx.author.id:
             modal = TagEditModal(self._db, self._tag)
             await interaction.response.send_modal(modal)
         else:
-            await interaction.response.send_message("Fool", ephemeral=True)
+            await interaction.response.send_message("You cannot edit this tag", ephemeral=True)
 
 
 class TagCreationModal(nextcord.ui.Modal):
-    def __init__(self, db: psycopg2.connection):
-        self._db: psycopg2.connection = db
+    def __init__(self, db):
+        self._db = db
         super().__init__("Create tag")
 
         self.my_name = nextcord.ui.TextInput(
@@ -113,7 +95,6 @@ class TagCreationModal(nextcord.ui.Modal):
             max_length=60,
             style=nextcord.TextInputStyle.short,
         )
-
         self.add_item(self.my_name)
 
         self.my_title = nextcord.ui.TextInput(
@@ -133,239 +114,134 @@ class TagCreationModal(nextcord.ui.Modal):
         self.add_item(self.my_content)
 
     async def callback(self, interaction: nextcord.Interaction) -> None:
-        with self._db.cursor() as cursor:
-            cursor.execute("SELECT * FROM taginfo WHERE name=%s", (self.my_name.value,))
-            if not cursor.fetchone():
-                id = uuid.uuid4()
-                try:
-                    cursor.execute(
-                        # f"INSERT INTO taginfo VALUES('{id.hex}', '{self.my_name.value}', '{self.my_title.value}', '{self.my_content.value}', '{str(interaction.user.id)}')"
-                        "INSERT INTO taginfo VALUES(%s, %s, %s, %s, %s)",
-                        (
-                            id.hex,
-                            self.my_name.value,
-                            self.my_title.value,
-                            self.my_content.value,
-                            str(interaction.user.id),
-                        ),
-                    )
-                except:
-                    cursor.execute("ROLLBACK")
-                self._db.commit()
-                await interaction.response.send_message("Done", ephemeral=True)
-            else:
-                await interaction.response.send_message(
-                    "Tag already existed", ephemeral=True
-                )
-
-        # await interaction.response.send_message("Done", ephemeral=True)
+        existing_tag = await self._db.tags.find_one({"name": self.my_name.value})
+        if not existing_tag:
+            tag_id = uuid.uuid4().hex
+            await self._db.tags.insert_one({
+                "_id": tag_id,
+                "name": self.my_name.value,
+                "title": self.my_title.value,
+                "content": self.my_content.value,
+                "author_id": str(interaction.user.id),
+            })
+            await interaction.response.send_message("Tag created successfully", ephemeral=True)
+        else:
+            await interaction.response.send_message("Tag already exists", ephemeral=True)
 
 
 class TagCreationView(nextcord.ui.View):
-    def __init__(self, ctx: commands.Context, db: psycopg2.connection):
+    def __init__(self, ctx: commands.Context, db):
         super().__init__()
-        self._ctx: commands.Context = ctx
-        self._db: psycopg2.connection = db
+        self._ctx = ctx
+        self._db = db
 
     @nextcord.ui.button(label="Create tag!", style=nextcord.ButtonStyle.green)
-    async def create_tag(
-        self, button: nextcord.ui.Button, interaction: nextcord.Interaction
-    ):
+    async def create_tag(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
         if interaction.user.id == self._ctx.author.id:
             modal = TagCreationModal(self._db)
             await interaction.response.send_modal(modal)
         else:
-            await interaction.response.send_message("Fool", ephemeral=True)
-
-
-def tag_operation_check(ctx):
-    return (
-        ctx.author.get_role(1197475623745110109) is not None
-    ) or ctx.author.id == 716134528409665586
+            await interaction.response.send_message("You cannot create this tag", ephemeral=True)
 
 
 class TagsNewSlash(commands.Cog):
     def __init__(self, bot: commands.Bot):
-        self._bot: commands.Bot = bot
-        self._db = psycopg2.connection = psycopg2.connect(
-            host=getenv("DBHOST"),
-            user=getenv("DBUSER"),
-            port=getenv("DBPORT"),
-            password=getenv("DBPASSWORD"),
-            dbname=getenv("DBNAME"),
-        )
+        self._bot = bot
+        # MongoDB connection
+        self._db = AsyncIOMotorClient(os.getenv("MONGO")).get_database("anna")
 
     @nextcord.slash_command()
     async def tag(self, interaction: nextcord.Interaction) -> None:
         pass
 
     @tag.subcommand()
-    @application_checks.has_role(1197475623745110109)
+    @application_checks.has_role(1281898369245253741)
     async def create(self, interaction: nextcord.Interaction) -> None:
         await interaction.response.send_modal(TagCreationModal(self._db))
 
     @tag.subcommand()
-    @application_checks.has_role(1197475623745110109)
-    async def edit(
-        self,
-        interaction: nextcord.Interaction,
-        tag_name: str = nextcord.SlashOption(
-            description="The tag you want",
-            required=True,
-        ),
-    ) -> None:
-        """Edit a tag"""
-        with self._db.cursor() as cursor:
-            cursor.execute("SELECT * FROM taginfo\nWHERE name=%s", (tag_name,))
-            if info := cursor.fetchone():
-                await interaction.response.send_modal(TagEditModal(self._db, info))
-            else:
-                await interaction.response.send_message("Invalid tag", ephemeral=True)
+    @application_checks.has_role(1281898369245253741)
+    async def edit(self, interaction: nextcord.Interaction, tag_name: str) -> None:
+        tag = await self._db.tags.find_one({"name": tag_name})
+        if tag:
+            await interaction.response.send_modal(TagEditModal(self._db, tag))
+        else:
+            await interaction.response.send_message("Tag not found", ephemeral=True)
 
     @tag.subcommand()
-    async def find(
-        self,
-        interaction: nextcord.Interaction,
-        tag_name: str = nextcord.SlashOption(
-            description="The tag you want",
-            required=True,
-        ),
-    ) -> None:
-        """Request a tag"""
-        with self._db.cursor() as cursor:
-            cursor.execute("SELECT * FROM taginfo\nWHERE name=%s", (tag_name,))
-            if info := cursor.fetchone() is not None:
-                # print(info)
-                await interaction.send(
-                    embed=nextcord.Embed(
-                        title=info[2], description=info[3], color=nextcord.Color.blue()
-                    ).set_footer(text=f"ID {info[0]}. Author ID: {info[4]}")
-                )
-            else:
-                await interaction.send("Fool")
+    async def find(self, interaction: nextcord.Interaction, tag_name: str) -> None:
+        tag = await self._db.tags.find_one({"name": tag_name})
+        if tag:
+            embed = nextcord.Embed(title=tag["title"], description=tag["content"], color=nextcord.Color.blue())
+            embed.set_footer(text=f"ID: {tag['_id']}, Author: {tag['author_id']}")
+            await interaction.send(embed=embed)
+        else:
+            await interaction.send("Tag not found", ephemeral=True)
 
     @tag.subcommand()
-    async def delete(
-        self,
-        interaction: nextcord.Interaction,
-        tag_name: str = nextcord.SlashOption(
-            description="The tag you want", required=True
-        ),
-    ) -> None:
-        """Delete a tag"""
-        with self._db.cursor() as cursor:
-            cursor.execute("SELECT * FROM taginfo WHERE name=%s", (tag_name,))
-            if cursor.fetchone() is not None:
-                cursor.execute("DELETE FROM taginfo WHERE name=%s", (tag_name,))
-                self._db.commit()
-                await interaction.send("Done")
-            else:
-                await interaction.send("Fool")
+    async def delete(self, interaction: nextcord.Interaction, tag_name: str) -> None:
+        tag = await self._db.tags.find_one({"name": tag_name})
+        if tag:
+            await self._db.tags.delete_one({"name": tag_name})
+            await interaction.send("Tag deleted successfully")
+        else:
+            await interaction.send("Tag not found")
 
 
 class TagsNew(commands.Cog):
     def __init__(self, bot: commands.Bot):
-        self._bot: commands.Bot = bot
-        self._db: psycopg2.connection = psycopg2.connect(
-            host=getenv("DBHOST"),
-            user=getenv("DBUSER"),
-            port=getenv("DBPORT"),
-            password=getenv("DBPASSWORD"),
-            dbname=getenv("DBNAME"),
-        )
+        self._bot = bot
+        self._db = AsyncIOMotorClient(os.getenv("MONGO")).get_database("anna")
 
     @commands.Cog.listener()
     async def on_message(self, message: nextcord.Message) -> None:
         if message.content.startswith("^"):
-            if os.getenv("NO_SPAWN_TAG") == 1:
-                return
-            with self._db.cursor() as cursor:
-                cursor.execute(
-                    "SELECT * FROM taginfo\nWHERE name=%s",
-                    (message.content[1:],),
-                )
-                if info := cursor.fetchone():
-                    # print(info)
-                    await message.channel.send(
-                        embed=nextcord.Embed(
-                            title=info[2],
-                            description=info[3],
-                            color=nextcord.Color.blue(),
-                        ).set_footer(text=f"ID {info[0]}. Author ID: {info[4]}")
-                    )
-                else:
-                    pass
+            tag_name = message.content[1:]
+            tag = await self._db.tags.find_one({"name": tag_name})
+            if tag:
+                embed = nextcord.Embed(title=tag["title"], description=tag["content"], color=nextcord.Color.blue())
+                embed.set_footer(text=f"ID: {tag['_id']}, Author: {tag['author_id']}")
+                await message.channel.send(embed=embed)
 
     @commands.group(invoke_without_command=True)
     async def tag(self, ctx: commands.Context, tag_name: str = "null"):
-        """Find a tag. Equivalent to `^tag_name` and `oc/find tag_name`."""
-        with self._db.cursor() as cursor:
-            cursor.execute("SELECT * FROM taginfo\nWHERE name=%s", (tag_name,))
-            if info := cursor.fetchone():
-                # print(info)
-                await ctx.send(
-                    embed=nextcord.Embed(
-                        title=info[2], description=info[3], color=nextcord.Color.blue()
-                    ).set_footer(text=f"ID {info[0]}. Author ID: {info[4]}")
-                )
-            else:
-                await ctx.send("Fool")
+        tag = await self._db.tags.find_one({"name": tag_name})
+        if tag:
+            embed = nextcord.Embed(title=tag["title"], description=tag["content"], color=nextcord.Color.blue())
+            embed.set_footer(text=f"ID: {tag['_id']}, Author: {tag['author_id']}")
+            await ctx.send(embed=embed)
+        else:
+            await ctx.send("Tag not found")
 
     @tag.command()
     async def list(self, ctx: commands.Context):
-        """List current available tag."""
-        with self._db.cursor() as cursor:
-            cursor.execute("SELECT name FROM taginfo")
-            await ctx.send(cursor.fetchall())
-
-    @commands.command()
-    async def find(self, ctx: commands.Context, tag_name: str = "null"):
-        """Find a tag."""
-        # print("command found")
-        # print(tag_name)
-        with self._db.cursor() as cursor:
-            cursor.execute("SELECT * FROM taginfo\nWHERE name=%s", (tag_name,))
-            if info := cursor.fetchone():
-                # print(info)
-                await ctx.send(
-                    embed=nextcord.Embed(
-                        title=info[2], description=info[3], color=nextcord.Color.blue()
-                    ).set_footer(text=f"ID {info[0]}. Author ID: {info[4]}")
-                )
-            else:
-                await ctx.send("Fool")
+        tags = await self._db.tags.find().to_list(length=100)
+        tag_names = [tag["name"] for tag in tags]
+        await ctx.send(", ".join(tag_names))
 
     @tag.command()
-    @commands.check(tag_operation_check)
+    @commands.check(lambda ctx: ctx.author.id == 716134528409665586)
     async def create(self, ctx: commands.Context):
-        """Create a tag."""
         await ctx.send(view=TagCreationView(ctx, self._db))
 
     @tag.command()
-    @commands.check(tag_operation_check)
+    @commands.check(lambda ctx: ctx.author.id == 716134528409665586)
     async def delete(self, ctx: commands.Context, tag_name: str):
-        """Delete a tag."""
-        with self._db.cursor() as cursor:
-            cursor.execute("SELECT * FROM taginfo WHERE name=%s", (tag_name,))
-            if cursor.fetchone():
-                cursor.execute("DELETE FROM taginfo WHERE name=%s", (tag_name,))
-                self._db.commit()
-                await ctx.send("Done")
-            else:
-                await ctx.send("Fool")
+        tag = await self._db.tags.find_one({"name": tag_name})
+        if tag:
+            await self._db.tags.delete_one({"name": tag_name})
+            await ctx.send("Tag deleted")
+        else:
+            await ctx.send("Tag not found")
 
     @tag.command()
-    @commands.check(tag_operation_check)
+    @commands.check(lambda ctx: ctx.author.id == 716134528409665586)
     async def edit(self, ctx: commands.Context, tag_name: str):
-        """Edit a tag."""
-        with self._db.cursor() as cursor:
-            cursor.execute("SELECT * FROM taginfo WHERE name=%s", (tag_name,))
-            if info := cursor.fetchone():
-                await ctx.send(
-                    f"Editing tag {tag_name}", view=TagEditView(ctx, self._db, info)
-                )
-            else:
-                await ctx.send("Fool")
+        tag = await self._db.tags.find_one({"name": tag_name})
+        if tag:
+            await ctx.send(f"Editing tag {tag_name}", view=TagEditView(ctx, self._db, tag))
+        else:
+            await ctx.send("Tag not found")
 
 
 def setup(bot):
