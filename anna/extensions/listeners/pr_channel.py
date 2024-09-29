5baf559aeeac842e0e62f2fbb252fefae20eca03
orxngc <orangc@proton.me>
2024-09-29 09:41:24 +0300

    fixx isadev pr channel

---
+++ anna/extensions/listeners/pr_channel.py
@@ -0,0 +1,32 @@
+import re
+import nextcord
+from nextcord.ext import commands
+
+GITHUB_URL_PATTERN = r"https:\/\/github.com\/([A-Za-z0-9-]+)\/([A-Za-z0-9-]+)(\/pull|\/issues)?(#|\/)(?P<pr_id>\d+)"
+SHORT_PR_PATTERN = r"##(\d+)"
+
+class PRChannelMessageCleaner(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        self.target_channel_id = 1130858271620726784
+        self.exempt_role_id = 1130858271620726784
+
+    @commands.Cog.listener()
+    async def on_message(self, message: nextcord.Message):
+        if message.author.bot:
+            return
+
+        if message.channel.id != self.target_channel_id:
+            return
+
+        if any(role.id == self.exempt_role_id for role in message.author.roles):
+            return
+
+        if re.search(GITHUB_URL_PATTERN, message.content) or re.search(SHORT_PR_PATTERN, message.content):
+            return 
+        
+        await message.delete()
+
+
+def setup(bot: commands.Bot):
+    bot.add_cog(PRChannelMessageCleaner(bot))
