bdc2d0c575eca5c60d6356d2cbd849f60855eb57
orangci <orangc@proton.me>
2024-12-24 19:22:25 +0300

    feat: rewrite ping iostpa cog

---
+++ anna/extensions/listeners/ping_iostpa.py
@@ -0,0 +1,21 @@
+from nextcord.ext import commands
+import nextcord
+
+SERVER_ID = 1281898369236602903 # real one → 830872854677422150
+
+def is_in_guild():
+    def predicate(ctx: commands.Context):
+        return ctx.guild and ctx.guild.id == SERVER_ID
+    return commands.check(predicate)
+
+
+class PingIostpa(commands.Cog):
+    @commands.Cog.listener("on_message")
+    async def ping_cutedog(self, message: nextcord.Message) -> None:
+        if message.guild.id != SERVER_ID:
+            return
+        if 740117772566265876 in [pong.id for pong in message.mentions]:
+            await message.channel.send("<@716306888492318790>, you were pinged on your alt account.")
+
+def setup(bot):
+    bot.add_cog(PingIostpa())
